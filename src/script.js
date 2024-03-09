c5e8d2a07ef2c429feca3ab013cf9230cbdc283f
Vegard-hd <vegard.hd@outlook.com>
2024-03-09 23:41:50 +0100

    added a grid container + gsap animation when createRow gets called

--- src/script.js
+++ src/script.js
@@ -4,6 +4,7 @@ const lemon = `../assets/fruit-limao-limon-svgrepo-com.svg`;
 const orange = `../assets/fruit-laranja-orange-svgrepo-com.svg`;
 const apple = `../assets/apple-apples-fruit-svgrepo-com.svg`;
 const plum = `../assets/ameixa-fruit-plum-svgrepo-com.svg`;
+
 function randomFruit() {
 	let fruit;
 	let rNum = Math.floor(Math.random() * 6 + 1);
@@ -31,7 +32,6 @@ function randomFruit() {
 	return fruit;
 }
 
-randomFruit();
 // replaces img link in original with random fruits
 $("#original")
 	.children()
@@ -39,15 +39,37 @@ $("#original")
 		// $(valueOfElement).attr("class");
 		$(valueOfElement).attr("src", randomFruit);
 	});
-function creatRow() {
-	//clones the original div, removes its attribute then calls randomfruit() for each child.
-	let divCopy = $("#original").clone().attr("id", "").appendTo("main");
+
+function createRow() {
+	// Clones the original div, removes its ID attribute, and calls randomFruit() for each child.
+	let divCopy = $("#original")
+		.clone()
+		.attr("id", "")
+		.prependTo(".container-md");
+
+	// Apply a fade-in animation to the cloned row
+	gsap.from(divCopy, {
+		y: "-100%",
+		opacity: 0,
+		duration: 1,
+		ease: "expo.in",
+	});
+
 	$(divCopy)
 		.children()
 		.each(function (indexInArray, valueOfElement) {
 			$(valueOfElement).attr("src", randomFruit);
 		});
+	let arr1 = [];
+	$("main div").each(function (index, element) {
+		// element == this
+		arr1.push(element);
+	});
+	console.log(arr1);
+	// if (arr1.length <= 8) createRow();
 }
+createRow();
 
-creatRow();
-creatRow();
+// Example usage:
+
+// gsap.
