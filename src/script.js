dc1c4d74e837be7e4da330d515f008ab36a14383
Vegard-hd <vegard.hd@outlook.com>
2024-03-09 23:03:12 +0100

    createRow now inserts content inside a div which is a copy of the original div.

--- src/script.js
+++ src/script.js
@@ -1,17 +1,9 @@
-console.log("test log");
-
 const pear = `../assets/fruit-pear-pears-svgrepo-com.svg`;
-
 const mango = `../assets/fruit-manga-mango-svgrepo-com.svg`;
-
 const lemon = `../assets/fruit-limao-limon-svgrepo-com.svg`;
-
 const orange = `../assets/fruit-laranja-orange-svgrepo-com.svg`;
-
 const apple = `../assets/apple-apples-fruit-svgrepo-com.svg`;
-
 const plum = `../assets/ameixa-fruit-plum-svgrepo-com.svg`;
-
 function randomFruit() {
 	let fruit;
 	let rNum = Math.floor(Math.random() * 6 + 1);
@@ -44,16 +36,17 @@ randomFruit();
 $("#original")
 	.children()
 	.each(function (indexInArray, valueOfElement) {
+		// $(valueOfElement).attr("class");
 		$(valueOfElement).attr("src", randomFruit);
 	});
 function creatRow() {
-	$("#original")
-		.clone()
+	//clones the original div, removes its attribute then calls randomfruit() for each child.
+	let divCopy = $("#original").clone().attr("id", "").appendTo("main");
+	$(divCopy)
 		.children()
 		.each(function (indexInArray, valueOfElement) {
 			$(valueOfElement).attr("src", randomFruit);
-		})
-		.appendTo(".main");
+		});
 }
 
 creatRow();
