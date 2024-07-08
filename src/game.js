43aec9f04634ca9c3dbbd067533dd077c709efb4
Vegard-hd <vegard.hd@outlook.com>
2024-07-08 18:27:05 +0200

    remade the making of fruits; now uses populateGame function

--- src/game.js
+++ src/game.js
@@ -1,26 +1,76 @@
 import randomFruit from "./RandomFruit.js";
 $(function () {
-	let arrayOfFruitsOne = [];
-	function recursiveFruitPusher(input = []) {
-		if (input.length < 10) {
-			input.push(randomFruit());
-			recursiveFruitPusher(input);
+	function populateGame() {
+		if ($("#row1")[0].childNodes.length >= 10) return;
+		else {
+			$("#row1").prepend(randomFruit());
+			populateGame();
+		}
+		if ($("#row2")[0].childNodes.length >= 10) return;
+		else {
+			$("#row2").prepend(randomFruit());
+			populateGame();
+		}
+		if ($("#row3")[0].childNodes.length >= 10) return;
+		else {
+			$("#row3").prepend(randomFruit());
+			populateGame();
+		}
+		if ($("#row4")[0].childNodes.length >= 10) return;
+		else {
+			$("#row4").prepend(randomFruit());
+			populateGame();
+		}
+		if ($("#row5")[0].childNodes.length >= 10) return;
+		else {
+			$("#row5").prepend(randomFruit());
+			populateGame();
+		}
+
+		if ($("#row6")[0].childNodes.length >= 10) return;
+		else {
+			$("#row6").prepend(randomFruit());
+			populateGame();
+		}
+		if ($("#row7")[0].childNodes.length >= 10) return;
+		else {
+			$("#row7").prepend(randomFruit());
+			populateGame();
+		}
+		if ($("#row8")[0].childNodes.length >= 10) return;
+		else {
+			$("#row8").prepend(randomFruit());
+			populateGame();
+		}
+		if ($("#row9")[0].childNodes.length >= 10) return;
+		else {
+			$("#row9").prepend(randomFruit());
+			populateGame();
+		}
+		if ($("#row10")[0].childNodes.length >= 10) return;
+		else {
+			$("#row10").prepend(randomFruit());
+			populateGame();
+		}
+		if ($("#row11")[0].childNodes.length >= 10) return;
+		else {
+			$("#row11").prepend(randomFruit());
+			populateGame();
+		}
+		if ($("#row12")[0].childNodes.length >= 10) return;
+		else {
+			$("#row12").prepend(randomFruit());
+			populateGame();
 		}
 	}
-	recursiveFruitPusher(arrayOfFruitsOne);
-
-	arrayOfFruitsOne.forEach((elmnt) => {
-		$("#row1").prepend(elmnt);
-		$("#row2").prepend(elmnt);
-		$("#row3").prepend(elmnt);
-		$("#row4").prepend(elmnt);
-		$("#row5").prepend(elmnt);
-		$("#row6").prepend(elmnt);
-		$("#row7").prepend(elmnt);
-		$("#row8").prepend(elmnt);
-		$("#row9").prepend(elmnt);
-		$("#row10").prepend(elmnt);
-		$("#row11").prepend(elmnt);
-		$("#row12").prepend(elmnt);
+	populateGame();
+	$("img").on("click", async function () {
+		await $(this).remove();
+		populateGame();
 	});
+	let row = $("#gridWrapper")[0].children;
+	$.each(row, function (indexInArray, valueOfElement) {
+		console.log(valueOfElement);
+	});
+	console.log(row);
 });
