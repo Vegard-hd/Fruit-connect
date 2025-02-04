f19d510899a47a58f968c6457835152f53176ea0
vegard-hd <vegard.hd@outlook.com>
2025-02-04 16:42:33 +0100

    updated project with supabase and express render function + bun build

--- app.js
+++ app.js
@@ -9,15 +9,23 @@ import { fileURLToPath } from "url";
 import morgan from "morgan";
 import compression from "compression";
 import { serialize, parse } from "cookie";
-
+import supabase from "./supabase";
 const { randomUUID } = new ShortUniqueId({
   length: 10,
 });
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 // Initialize Express app and services
 const app = express();
 
-app.use(morgan("tiny"));
+var completedRouter = require("./routes/completed");
+var indexRouter = require("./routes/index");
+// view engine setup
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
+
+app.use(morgan("dev"));
 app.use(compression());
 
 const server = createServer(app);
@@ -27,8 +35,6 @@ const io = new Server(server, {
 const fruitService = new FruitService();
 
 // Get the directory name using ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 // Serve static files
 app.use("/public", express.static(path.join(__dirname, "public")));
@@ -36,12 +42,30 @@ app.use(
   "/public/js",
   express.static(path.join(__dirname, "node_modules/socket.io/client-dist"))
 );
+app.use(
+  "/public/js",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/js"))
+);
+app.use(
+  "/public/css",
+  express.static(path.join(__dirname, "node_modules/bootstrap/dist/css"))
+);
+app.use(
+  "/public/js/jquery.slim.min.js",
+  express.static(
+    path.join(__dirname, "node_modules/jquery/dist/jquery.slim.min.js")
+  )
+);
+
 app.use("/favicon.ico", express.static(path.join(__dirname, "favicon.ico")));
 
 // Serve index.html for root path
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
-});
+
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public", "index.html"));
+// });
+app.use("/", indexRouter);
+app.use("/completed", completedRouter);
 
 // Socket.IO connection handling
 io.on("connection", async (socket) => {
@@ -73,7 +97,20 @@ io.on("connection", async (socket) => {
     socket.on("message", async (message) => {
       try {
         const [result, score] = await calculateFruitsDfs(message, userId);
+
         if (scoreCount >= 10) {
+          const { data, error } = await supabase
+            .from("completedGames")
+            .insert([{ score: userScore, gameId: userId, username: "admin" }])
+            .select();
+
+          if (error)
+            throw new Error(
+              "Something went wrong with writing to the database"
+            );
+          socket.emit("message", { gameEnded: true, data: data });
+          userScore = 0;
+          scoreCount = 0;
           //write to DB
           //end game
           //reset userScore and scoreCount
@@ -102,6 +139,16 @@ io.on("connection", async (socket) => {
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get("env") === "development" ? err : {};
+  // render the error page
+  res.status(err.status || 500);
+  res.render("error");
 });
 
 export default app;
