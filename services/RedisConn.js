9fb6e20273a5cff4195246881bb0a1c2fec34d0d
vegard-hd <vegard.hd@outlook.com>
2025-02-15 10:57:44 +0100

    changes to redis docker setup

--- services/RedisConn.js
+++ services/RedisConn.js
@@ -1,6 +1,6 @@
 import { createClient } from "redis";
 
-const redisClient = await createClient()
+const redisClient = await createClient({ url: "redis://redis:6379" })
   .on("error", (err) => console.log("Redis Client Error", err))
   .connect();
 
